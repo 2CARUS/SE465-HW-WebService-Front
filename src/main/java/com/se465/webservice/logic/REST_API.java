e75fcf8bf7516ca95f9699caefdc9ae14d696796
2CARUS <c.kopp.professional@gmail.com>
2019-06-23 20:36:14 -0500

    readded rest_api

---
+++ src/main/java/com/se465/webservice/logic/REST_API.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.se465.webservice.logic;
+
+/**
+ *
+ * @author ckopp
+ */
+public interface REST_API {
+
+    String URL = "https://se465-hw-inventory.azurewebsites.net/api";
+    String MPLS = "/minneapolisitems";
+    String STPL = "/saintpaulitems";
+
+}
